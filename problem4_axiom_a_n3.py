4f924a448f8338c56aa0d2e5ef2e1bd2c008a363
Meriem Dhouibi <159493224+meriemdhouibi13@users.noreply.github.com>
2025-10-22 20:43:04 -0500

    Create new file problem4_axiom_a_n3.py

---
+++ problem4_axiom_a_n3.py
@@ -0,0 +1,21 @@
+# Finding a model for axioms A.1-A.4 when n=3
+
+# This script aims to provide a model that satisfies the axioms A.1 to A.4.
+
+# Axiom A.1: [Description of Axiom A.1]
+# Axiom A.2: [Description of Axiom A.2]
+# Axiom A.3: [Description of Axiom A.3]
+# Axiom A.4: [Description of Axiom A.4]
+
+# Define the model for n=3
+n = 3
+
+# [Implementation of the model]
+
+# Example implementation
+# def model_for_axioms(n):
+#     # Logic to find the model
+#     pass
+
+# if __name__ == '__main__':
+#     model_for_axioms(n)
